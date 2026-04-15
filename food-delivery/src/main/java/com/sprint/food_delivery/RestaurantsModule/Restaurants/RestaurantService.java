8742d5846440306045597aa138dac623922d1811
kisol <kisolshamilishaj@gmail.com>
2026-04-15 14:51:13 +0530

    error cleared

--- food-delivery/src/main/java/com/sprint/food_delivery/RestaurantsModule/Restaurants/RestaurantService.java
+++ food-delivery/src/main/java/com/sprint/food_delivery/RestaurantsModule/Restaurants/RestaurantService.java
@@ -5,8 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sprint.food_delivery.entity.Restaurants;
-import com.sprint.food_delivery.repository.RestaurantsRepository;
+
 
 @Service
 public class RestaurantService {
--- food-delivery/src/main/java/com/sprint/food_delivery/RestaurantsModule/Restaurants/RestaurantsRepository.java
+++ food-delivery/src/main/java/com/sprint/food_delivery/RestaurantsModule/Restaurants/RestaurantsRepository.java
@@ -3,7 +3,7 @@ package com.sprint.food_delivery.RestaurantsModule.Restaurants;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sprint.food_delivery.entity.Restaurants;
+
 
 public interface RestaurantsRepository extends JpaRepository<Restaurants, Integer>{
 
