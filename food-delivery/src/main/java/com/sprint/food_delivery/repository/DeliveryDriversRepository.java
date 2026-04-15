73099ccdc6407e3a87697b40f8096f157194476c
Annie Rufina <anniecharles2004@gmail.com>
2026-04-15 08:42:00 +0530

    updated repository

---
+++ food-delivery/src/main/java/com/sprint/food_delivery/repository/DeliveryDriversRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.food_delivery.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sprint.food_delivery.entity.DeliveryDrivers;
+
+public interface DeliveryDriversRepository extends JpaRepository<DeliveryDrivers, Integer> {
+
+}
