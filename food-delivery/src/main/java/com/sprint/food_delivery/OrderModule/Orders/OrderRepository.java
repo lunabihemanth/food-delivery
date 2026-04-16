37cf69c6dcd883f08c480fa250db6441acea7869
kisol <kisolshamilishaj@gmail.com>
2026-04-16 12:51:06 +0530

    kisol is ready

--- food-delivery/src/main/java/com/sprint/food_delivery/OrderModule/Orders/OrderRepository.java
+++ food-delivery/src/main/java/com/sprint/food_delivery/OrderModule/Orders/OrderRepository.java
@@ -1,13 +1,16 @@
 package com.sprint.food_delivery.OrderModule.Orders;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sprint.food_delivery.OrderModule.Orders.Orders;
 
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
 
-	List<Orders> findByDeliveryDriverDriverId(Integer driverId);
+    
+    List<Orders> findByDeliveryDriverDriverId(Integer driverId);
 
+   
+    List<Orders> findByCustomerCustomerId(Integer customerId);
+
+    
+    List<Orders> findByRestaurantRestaurantId(Integer restaurantId);
 }
